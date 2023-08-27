b064cb6ac40e076a561a05276fe60ae685be63a3
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu>
2023-08-27 17:55:28 +0100

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global user.email "ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz"
+git config --global user.email "ahmed_abd_el_mounaim.belkhoumali@g.enp.edu"
 git config --global user.name "belkhoumali-Ahmed-Abdelmounaim"
 
 git remote add origin https://github.com/belkhoumali-Ahmed-Abdelmounaim/Data_Sage.git   # if origin already exists, skip this line
