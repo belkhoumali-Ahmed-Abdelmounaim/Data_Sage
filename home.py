ee04b0ccf5b530e49c09b12d56eb598813107264
belkhoumali-Ahmed-Abdelmounaim <89152218+belkhoumali-Ahmed-Abdelmounaim@users.noreply.github.com>
2023-12-13 12:33:01 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 from pandasai.llm.openai import OpenAI
-from pandasai.pandasai import PandasAI  # Add this import
 import openai
 import os
 import matplotlib.pyplot as plt
